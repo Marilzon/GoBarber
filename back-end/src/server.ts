a5c6eaea1615869cda3942e126e833e7f16a3140
Marilzon <maxmaril@hotmail.com>
2020-10-30 05:57:57 -0300

    Feature: debugging code via vscode debugger

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,11 +1,12 @@
-import express from 'express';
+import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello marilzon' });
+app.get("/", (req, res) => {
+    return res.json({ message: "Hello marilzon" });
 });
 
 app.listen(3333, () => {
-    console.log("Server list on port 3333!")
+    console.log("Server list on port 3333!");
 });
