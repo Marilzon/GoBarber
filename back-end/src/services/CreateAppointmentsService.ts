ecc1b07ae3c4e6d3ede12f21698a090afbfa3f25
Marilzon <maxmaril@hotmail.com>
2020-10-30 19:25:28 -0300

    Feature: creating services, to organiza rules of bussiness of routes

---
+++ back-end/src/services/CreateAppointmentsService.ts
@@ -0,0 +1,35 @@
+import { startOfHour } from "date-fns";
+
+import Appointment from "../models/Appointment";
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
+
+interface RequestDTO {
+  provider: string;
+  date: Date;
+}
+
+class CreateAppointmentService {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+    this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({ date, provider }: RequestDTO): Appointment {
+    const appointmentDate = startOfHour(date);
+
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+      appointmentDate,
+    );
+
+    if (findAppointmentInSameDate) {
+      throw new Error("This appointment is already booked");
+    }
+
+    const appointment = this.appointmentsRepository.create({ provider, date });
+
+    return appointment;
+  }
+}
+
+export default CreateAppointmentService;
