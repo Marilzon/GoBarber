2adb8a9faaf036fcac78a365dc223b77563d74ef
Marilzon <maxmaril@hotmail.com>
2020-11-05 18:47:02 -0300

    Feature: validate inputs with You lib

--- front-end/src/pages/SignUp/index.tsx
+++ front-end/src/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiLock, FiUser, FiMail } from 'react-icons/fi';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
 
 import logoSvg from '../../assets/logo.svg';
 
@@ -10,9 +11,22 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: string): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: string) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('E-Mail obrigatório')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No minimo 6 digitos'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <Container>
