2a30f25039e31fd24482ec72de72126742f53801
Marilzon <maxmaril@hotmail.com>
2020-11-06 00:36:07 -0300

    Feature: apply rule to listen user input errors

--- front-end/src/pages/SignUp/index.tsx
+++ front-end/src/pages/SignUp/index.tsx
@@ -1,7 +1,9 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiLock, FiUser, FiMail } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoSvg from '../../assets/logo.svg';
 
@@ -11,8 +13,13 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  console.log(formRef);
+
   const handleSubmit = useCallback(async (data: string) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
@@ -24,7 +31,9 @@ const SignUp: React.FC = () => {
         abortEarly: false,
       });
     } catch (err) {
-      console.log(err);
+      const errors = getValidationErrors(err);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -33,10 +42,10 @@ const SignUp: React.FC = () => {
       <Background />
       <Content>
         <img src={logoSvg} alt="GoBarber" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
-          <Input name="name" icon={FiUser} placeholder="E-Mail" />
+          <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-Mail" />
           <Input
             name="password"
