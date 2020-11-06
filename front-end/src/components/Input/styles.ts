61453cf6ad9c27a6f9a83b401b863acb6bb56a5e
Marilzon <maxmaril@hotmail.com>
2020-11-06 03:22:31 -0300

    Feature: tolltip for errors

--- front-end/src/components/Input/styles.ts
+++ front-end/src/components/Input/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components';
+import Tolltip from '../Tolltip';
 
 interface ContainerProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -22,6 +24,12 @@ export const Container = styled.div<ContainerProps>`
   }
 
   ${props =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
+
+  ${props =>
     props.isFocused &&
     css`
       border-color: #ff9000;
@@ -31,7 +39,7 @@ export const Container = styled.div<ContainerProps>`
   ${props =>
     props.isFilled &&
     css`
-      color: #ff9000;
+      background-color: #ff9000;
     `}
 
   input {
@@ -47,5 +55,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tolltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #f4ede8;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
---
+++ front-end/src/components/Tolltip/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+interface TolltipProps {
+  title: string;
+  className?: string;
+}
+
+const Tolltip: React.FC<TolltipProps> = ({
+  title,
+  className = '',
+  children,
+}) => {
+  return (
+    <Container className={className}>
+      {children}
+      <span>{title}</span>
+    </Container>
+  );
+};
+export default Tolltip;
