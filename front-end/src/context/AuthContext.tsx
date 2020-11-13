63a21e40ec11d94595cef4127909ccbe7c2ad428
Marilzon <maxmaril@hotmail.com>
2020-11-13 17:51:01 -0300

    Feature: localstorage manipulation

--- front-end/src/context/AuthContext.tsx
+++ front-end/src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
+import { tokenToString } from 'typescript';
 import api from '../services/api';
 
 interface SignInCredentials {
@@ -7,8 +8,13 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  name: string;
+  user: string;
   signIn(credentials: SignInCredentials): Promise<void>;
+}
+
+interface AuthState {
+  token: string;
+  user: string;
 }
 
 export const AuthContext = createContext<AuthContextData>(
@@ -16,17 +22,33 @@ export const AuthContext = createContext<AuthContextData>(
 );
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@Gobarber:token');
+    const user = localStorage.getItem('@Gobarber:user');
+
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
+    }
+
+    return {} as AuthState;
+  });
+
   const signIn = useCallback(async ({ email, password }) => {
     const response = await api.post('sessions', {
       email,
       password,
     });
 
-    console.log(response.data);
+    const { token, user } = response.data;
+
+    localStorage.setItem('@Gobarber:token', token);
+    localStorage.setItem('@Gobarber:user', JSON.stringify(user));
+
+    setData({ token, user });
   }, []);
 
   return (
-    <AuthContext.Provider value={{ name: 'Marilzon', signIn }}>
+    <AuthContext.Provider value={{ user: data.user, signIn }}>
       {children}
     </AuthContext.Provider>
   );
