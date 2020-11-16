390b15e6e6e3ccc6bb5964ccc6d6ef8be9905a94
Marilzon <maxmaril@hotmail.com>
2020-11-16 17:48:36 -0300

    Fix: eslint/prettier conflict

--- front-end/src/routes/index.tsx
+++ front-end/src/routes/index.tsx
@@ -5,10 +5,14 @@ import { Switch, Route } from 'react-router-dom';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
+import Dashboard from '../pages/Dashboard';
+
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
 
